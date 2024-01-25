ae3befa5cc6574f68277620105cab7607acf7cb8
Bob K <rwkramer@ysu.edu>
2024-01-25 17:24:34 -0500

    added files for debugging demo

---
+++ Labs/Lab 3 - Debugging/Fraction/cmake-build-debug/CMakeFiles/Fraction.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bob/School/Courses/3700.course/Labs/03a - Debugging/Fraction/fraction.cpp" "/home/bob/School/Courses/3700.course/Labs/03a - Debugging/Fraction/cmake-build-debug/CMakeFiles/Fraction.dir/fraction.cpp.o"
+  "/home/bob/School/Courses/3700.course/Labs/03a - Debugging/Fraction/main.cpp" "/home/bob/School/Courses/3700.course/Labs/03a - Debugging/Fraction/cmake-build-debug/CMakeFiles/Fraction.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Labs/Lab 3 - Debugging/Fraction/cmake-build-debug/CMakeFiles/Fraction.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Fraction.dir/fraction.cpp.o"
+  "CMakeFiles/Fraction.dir/main.cpp.o"
+  "Fraction"
+  "Fraction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Fraction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
